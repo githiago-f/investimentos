09938f9de5250282af7c4c420163ebfc01bcf82f
Thiago Farias <52945005+githiago-f@users.noreply.github.com>
2021-11-06 15:10:49 -0300

    Update index.tsx

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -12,7 +12,7 @@ export const Container: FC<{}> = (props) => {
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
           <h1 className="text-3xl font-bold text-gray-900">
-            Bem vindo(a),<wbr/> {user.nome}!
+            Bem vindo(a),<wbr/> {user?.nome||'Usuário'}!
           </h1>
           <button className="logout" onClick={logout}>Logout</button>
         </div>
